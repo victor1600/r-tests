e8d0007edfc1365bbd3dea90829f20381f0bf10f
victor1600 <ledzepp1600@gmail.com>
2020-02-08 00:57:08 -0600

    test

--- main.R
+++ main.R
@@ -1,1 +1,79 @@
-x = scan()
+# Title     : TODO
+# Objective : TODO
+# Created by: victor
+# Created on: 7/2/20
+
+# Get help of desired function
+?ls
+
+# Run code
+# Select desired code and hit ctr+enter
+
+# Crear objeto
+paly = 5
+paly
+# Create random numbers using normal distribution
+# how much numbers is the parameter
+kiwi = 5 + rnorm(1)
+kiwi
+
+# Get the same number
+# set.seed(10) Will return the random number in index 10
+set.seed(100)
+kiwi<-5+rnorm(10)
+kiwi
+
+# Creating and array
+kiwi = c(1,2,3)
+kiwi
+
+# List created variables
+ls()
+
+# look al variables starting with k
+ls(pat='k')
+
+# Erase variable
+rm(kiwi)
+
+kiwi = 2
+paly = 5
+paly * kiwi
+
+# length of a variable
+length(kiwi)
+
+# variable's type
+mode(kiwi)
+
+# Get working directory
+getwd()
+
+# set workind directory
+wd = "/home/victor/courses/r-tests"
+setwd(wd)
+getwd()
+
+# list files
+list.files()
+
+# Import a library
+library(readxl)
+# Load an excel file
+excelTest = read_excel('testExcel.xlsx')
+excelTest
+
+ # SEQUENCES
+
+# Generate numbers
+poly = 1:30
+poly
+
+# sequence Define limits and setp
+seq(1,5,0.5)
+
+# 10 numbers from 2 to 15
+seq(length=10,from=2, to=15)
+
+c =scan()
+print(c)
--- md-files/basics.md
+++ md-files/basics.md
@@ -3,3 +3,30 @@
 Install from:
 > https://www.digitalocean.com/community/tutorials/how-to-install-r-on-ubuntu-18-04
 
+### Running code from terminal
+```shell script
+Rscript main.R
+```
+
+
+### Running Code in Pycharm
+Select code lines and hit ctrl + enter.
+
+### Creating  numbers using normal distribution
+
+* Create a random normal distribution of 1 element
+```r
+kiwi = 5 + rnorm(1)
+```
+
+* Create a random normal of n elements
+
+```r
+kiwi = 5+rnorm(10)
+```
+
+* Get random number of given index of random distribution
+
+```r
+
+```
